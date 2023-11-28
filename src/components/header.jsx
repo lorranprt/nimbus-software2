c0815c0944a7d301acbca6fd0898368b582390f2
Guilherme Vallim <guivallimaraujo@gmail.com>
2023-11-28 18:29:11 -0300

    commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 const HeaderContainer = styled.header`
   text-align: center;
@@ -7,10 +8,20 @@ const HeaderContainer = styled.header`
   height: 200px;
 `;
 
+const Botao = styled(Link)`
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px;
+  text-decoration: none;
+  border-radius: 5px;
+  display: inline-block;
+`;
+
 const Header = () => {
   return (
     <HeaderContainer>
       <img src={ require('./assets/logoNimbus.png') } style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+      <Botao to="/">Ir para Home</Botao>
     </HeaderContainer>
   );
 };
